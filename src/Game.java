1a0494c91721232a1239d6f6932bde1d716facf1
Ryan Boldi <ryan.boldi123@gmail.com>
2021-07-21 10:56:01 -0400

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -5,14 +5,31 @@ public class Game{
 
     Game() {
         board = new int[9];
+        board[1] = 1;
+        board[2] = -1;
         currentPlayer = 1; //X starts
+    }
+
+    /**
+     * gets 'X','O' or ' ' depending on which player is occupying this position
+     * @param x col that the player is in (0-indexed)
+     * @param y row that the player is in (0-indexed)
+     * @return one character 'X', 'O' or ' ';
+     */
+    private char getPlayerAt(int x, int y){
+        int player = board[(y*3) + x];
+        return (player == 1 
+        ? 'X' 
+        : player == -1
+            ? 'O' 
+            : ' ');
     }
 
     public String toString() {
         String soFar = " ";
         for (int i = 0; i < 3; i++){
             for (int j = 0; j < 3; j++){
-                soFar+=(board[(i * 3) + j]);
+                soFar+=getPlayerAt(j, i);
                 if (j < 2) soFar+=" | ";
             }
             soFar+="\n";
