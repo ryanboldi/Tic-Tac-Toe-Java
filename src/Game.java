28770c180c8357564ab19cda9ace292b5680a2c9
Ryan Boldi <ryan.boldi123@gmail.com>
2021-07-21 11:46:06 -0400

    added input parsing system

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,13 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
 public class Game{
     private int[] board; //game board
 
     private byte currentPlayer; //X is 1, O is -1
+
+    private static Scanner scanner = new Scanner(System.in);
 
     Game() {
         board = new int[9];
@@ -10,6 +16,55 @@ public class Game{
         currentPlayer = 1; //X starts
     }
 
+    /**
+     * String representation of the current Game Board
+     */
+    public String toString() {
+        String soFar = " ";
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                soFar+=getPlayerAt(j, i);
+                if (j < 2) soFar+=" | ";
+            }
+            soFar+="\n";
+            if (i < 2) soFar+="---+---+---\n ";
+        }
+
+        return soFar;
+    }
+
+    public void getNextTurn(){
+        System.out.println(String.format("Player: %c", getCurPlayer()));
+        System.out.println("Please select a board position to place at (x,y):");
+        boolean xValid = false;
+        int x;
+        while(!xValid){
+            System.out.print("x (1-3): ");
+            try {
+                x = scanner.nextInt();
+            } catch (InputMismatchException ex) {
+                System.out.println("Not a valid input. Please type and integer from 1 to 3");
+                scanner.nextLine();
+                continue;
+            }
+            xValid = true;
+            System.out.println("selected x: " + x);
+        }
+    }
+
+    private int getFromUser(String req, String errMsg, String var){
+        
+    }
+
+    private char getCurPlayer(){
+        return (currentPlayer == 1 
+        ? 'X' 
+        : currentPlayer == -1
+            ? 'O' 
+            : ' ');
+    }
+
+    
     /**
      * gets 'X','O' or ' ' depending on which player is occupying this position
      * @param x col that the player is in (0-indexed)
@@ -25,17 +80,14 @@ public class Game{
             : ' ');
     }
 
-    public String toString() {
-        String soFar = " ";
-        for (int i = 0; i < 3; i++){
-            for (int j = 0; j < 3; j++){
-                soFar+=getPlayerAt(j, i);
-                if (j < 2) soFar+=" | ";
-            }
-            soFar+="\n";
-            if (i < 2) soFar+="---+---+---\n ";
-        }
+    private int getNumAt(int x, int y) {return board[(y*3 + x)];}
 
-        return soFar;
+    private void placeAt(int x, int y) throws InvalidOperationError {
+        if (getNumAt(x, y) != 0){
+            throw new InvalidOperationError();
+        }
     }
 }
+
+
+class InvalidOperationError extends Exception{};
